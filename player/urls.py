d2de379e1202b173b3c83da9ad4bfedd111180c9
Vitalie Lazu <vitalie.lazu@gmail.com>
2018-06-08 19:42:25 +0300

    Use generic CreateView to signup a user

--- player/urls.py
+++ player/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import home, new_invitation, accept_invitation
+from .views import home, new_invitation, accept_invitation, SignUpView
 
 urlpatterns = [
   path('home', home, name="player_home"),
   path('login', LoginView.as_view(template_name="player/login_form.html"),
     name='player_login'
   ),
+  path('signup', SignUpView.as_view(), name='player_signup'),
   path('logout', LogoutView.as_view(), name="player_logout"),
   path('new_invitation', new_invitation, name="player_new_invitation"),
   path('accept_invitation/<int:id>', accept_invitation, name="player_accept_invitation"),
